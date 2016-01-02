baffbd566b3b599971f9d1b2910fdb298da42939
Vladimir <volkvid@yandex.ru>
2016-01-03 01:20:44 +0300

    improve formatting, all colorize to output;

--- main.py
+++ main.py
@@ -12,17 +12,29 @@ log = Logger("rates")
 
 class Adviser(object):
     CATEGORY = "DebitCardsOperations"
-    ALL_CURRENCIES = ("RUB", "USD", "EUR")
+    RUR_key = "RUB"
+    USD_key = "USD"
+    EUR_key = "EUR"
+    ALL_CURRENCIES = (RUR_key, USD_key, EUR_key)
+    COLORS = {
+        'pink': '\033[95m',
+        'blue': '\033[94m',
+        'green': '\033[92m',
+        'yellow': '\033[93m',
+        'red': '\033[91m',
+        'ENDC': '\033[0m',
+        'bold': '\033[1m',
+        'underline': '\033[4m'
+    }
 
     def __init__(self):
         self.url = "https://www.tinkoff.ru/api/v1/currency_rates/"
         with open("config.yaml", "r") as f:
             self.config = yaml.load(f)
+        self.rub_amount = self.config[self.RUR_key]
+        self.usd_amount = self.config[self.USD_key]
+        self.eur_amount = self.config[self.EUR_key]
         self.values = {}
-        for item in self.ALL_CURRENCIES:
-            self.values[item] = {
-                "my_amount": self.config.get(item)
-            }
 
     def __filter_data(self, data):
         """Removes extra data and filter all rates by currencies"""
@@ -46,73 +58,88 @@ class Adviser(object):
         except Exception as e:
             log.error("load_rates error: {}".format(repr(e)))
 
+    def str_color(self, color, data):
+        """Colorize output"""
+        return "%s%s%s" % (self.COLORS[color], data, self.COLORS['ENDC'])
+
     def help(self):
         """Calculate all variants"""
         self.__load_rates()
 
-        rub_amount = self.values["RUB"]["my_amount"]
-        usd_amount = self.values["USD"]["my_amount"]
-        eur_amount = self.values["EUR"]["my_amount"]
         all_cases = (
             {
                 "name": "EUR[RUB]",
                 "description": "Convert EUR to RUB",
                 "dimension": "RUB",
-                "value": eur_amount*self.values["EUR"]["RUB"]["buy"],
+                "value": self.eur_amount * self.values[self.EUR_key][self.RUR_key]["buy"],
             },
             {
                 "name": "USD[RUB]",
                 "description": "Convert USD to RUB",
                 "dimension": "RUB",
-                "value": usd_amount*self.values["USD"]["RUB"]["buy"],
+                "value": self.usd_amount * self.values[self.USD_key][self.RUR_key]["buy"],
             },
             {
                 "name": "EUR[USD]",
                 "description": "Convert EUR to USD",
                 "dimension": "USD",
-                "value": eur_amount*self.values["EUR"]["USD"]["buy"],
+                "value": self.eur_amount * self.values[self.EUR_key][self.USD_key]["buy"],
             },
             {
                 "name": "USD[EUR]",
                 "description": "Convert USD to EUR",
                 "dimension": "EUR",
-                "value": usd_amount*self.values["USD"]["EUR"]["buy"],
+                "value": self.usd_amount * self.values[self.USD_key][self.EUR_key]["buy"],
             },
             {
                 "name": "RUB[USD]",
                 "description": "Convert RUB to USD",
                 "dimension": "USD",
-                "value": rub_amount/self.values["USD"]["RUB"]["sell"],
+                "value": self.rub_amount / self.values[self.USD_key][self.RUR_key]["sell"],
             },
             {
                 "name": "RUB[USD]+EUR[USD]",
                 "description": "Convert RUB and EUR to USD",
                 "dimension": "USD",
-                "value": rub_amount/self.values["USD"]["RUB"]["sell"] + eur_amount*self.values["EUR"]["USD"]["buy"],
+                "value": self.rub_amount / self.values[self.USD_key][self.RUR_key]["sell"] +
+                         self.eur_amount * self.values[self.EUR_key][self.USD_key]["buy"],
             },
             {
                 "name": "RUB[EUR]+USD[EUR]",
                 "description": "Convert RUB and USD to EUR",
                 "dimension": "EUR",
-                "value": rub_amount/self.values["EUR"]["RUB"]["sell"] + usd_amount*self.values["USD"]["EUR"]["buy"],
+                "value": self.rub_amount / self.values[self.EUR_key][self.RUR_key]["sell"] +
+                         self.usd_amount * self.values[self.USD_key][self.EUR_key]["buy"],
             },
             {
                 "name": "(EUR[USD]+USD)[RUB]",
                 "description": "Total EUR and USD in USD",
                 "dimension": "RUB",
-                "value": (eur_amount*self.values["EUR"]["USD"]["buy"] + usd_amount)*self.values["USD"]["RUB"]["buy"],
+                "value": (self.eur_amount * self.values[self.EUR_key][self.USD_key]["buy"] + self.usd_amount) *
+                         self.values[self.USD_key][self.RUR_key]["buy"],
             },
             {
                 "name": "(USD[EUR]+EUR)[RUB]",
                 "description": "Total EUR and USD in EUR",
-                "dimension": "RUB",
-                "value": (usd_amount*self.values["USD"]["EUR"]["buy"] + eur_amount)*self.values["EUR"]["RUB"]["buy"],
+                "dimension": "RUB\n",  # print hack
+                "value": (self.usd_amount * self.values[self.USD_key][self.EUR_key]["buy"] + self.eur_amount) *
+                         self.values[self.EUR_key][self.RUR_key]["buy"],
             },
         )
+
+        log.info("\n\n\tUSD: {} / {}\n\tEUR: {} / {}\n".format(
+            self.str_color("green", self.values[self.USD_key][self.RUR_key]["buy"]),
+            self.str_color("red", self.values[self.USD_key][self.RUR_key]["sell"]),
+            self.str_color("green", self.values[self.EUR_key][self.RUR_key]["buy"]),
+            self.str_color("red", self.values[self.EUR_key][self.RUR_key]["sell"]),
+        ))
         for case in all_cases:
-            if "value" in case:
-                log.info("{} = {} {}".format(case["description"], round(case["value"], 3), case["dimension"]))
-        log.info("")
+            if "value" in case and not ("disabled" in case and not case["disabled"]):
+                log.info("{} = {} {}".format(
+                    case["description"],
+                    self.str_color("bold", round(case["value"], 3)),
+                    self.str_color("blue", case["dimension"])
+                ))
 
 
 if __name__ == "__main__":
